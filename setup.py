9bc2eb80f7baef4d10faf3f88bd9999bb63c362d
Mitchell Reynolds <mitchell-reynolds@M-Scott.local>
2023-03-24 14:39:08 -0700

    new project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Open Phil's AI Worldviews Contest & AI Governance Hackathon #6',
+    author='Mitchell Reynolds',
+    license='MIT',
+)
